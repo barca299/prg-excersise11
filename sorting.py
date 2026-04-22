b1c78b12db9cde4843476ead6bf8a3a713e2a6f3
barca299 <barakubcova@seznam.cz>
2026-04-22 09:04:04 +0200

    dd

--- sorting.py
+++ sorting.py
@@ -3,15 +3,21 @@ import random
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
+def selection_sort(values):
+    for j in range(len(values)):
+        min_indexy = j
+        min_values = values[min_indexy]
+        for i in range(j + 1, len(values)):
+            if values[i] < min_values:
+                min_indexy = i
+                min_values = values[i]
 
-
-
-
+        values[j], values[min_indexy] = values[min_indexy], values[j]
+        print(values)
+    return values
 
 
 #def main():
-
-
 
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
@@ -19,4 +25,7 @@ if __name__ == "__main__":
 
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
     print(small)
+
+    min_index = selection_sort(values)
     #main()
+
